fb576f6010cbe2f7e4fd0199949fc253031b279b
kakutagawa <kaktgw0824@gmail.com>
2024-02-01 08:40:53 +0900

    Initial Commit

---
+++ HOSHIIMONOLIST/HOSHIIMONOLISTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HOSHIIMONOLISTApp.swift
+//  HOSHIIMONOLIST
+//
+//  Created by 芥川浩平 on 2024/02/01.
+//
+
+import SwiftUI
+
+@main
+struct HOSHIIMONOLISTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
